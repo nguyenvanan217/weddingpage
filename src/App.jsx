5c36a8bcf416e0902db9ca6f805dcdba6d6681b6
An Nguyen <annguyen828721665@gmail.com>
2025-03-21 21:51:28 +0700

    thêm invitaiton tức là phầ (thư mời) + add loading trái tim

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../src/assets/logo.jpg';
 import Banner from './component/banner/Banner';
 import SnowEffect from './component/effect/SnowEffect';
 import './App.css';
 import Aside from './component/aside/Aside';
+import Invitationletter from './component/invitationletter/invitationletter';
+import Heart from './component/heart/heart';
+
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 4000); // 3 seconds
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="loading-screen">
+                <Heart />
+            </div>
+        );
+    }
+
     return (
         <div>
             <SnowEffect />
             <Banner />
             <Aside />
+            <Invitationletter />
         </div>
     );
 }
